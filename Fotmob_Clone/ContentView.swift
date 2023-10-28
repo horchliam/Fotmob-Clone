3b8e7244d96bb5a76f9433bc62bb7aa9128cbc60
horchliam <53147246+horchliam@users.noreply.github.com>
2023-10-28 15:51:57 -0400

    Initial Commit

---
+++ Fotmob_Clone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fotmob_Clone
+//
+//  Created by Liam Horch on 10/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fotmob_Clone/Fotmob_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fotmob_CloneApp.swift
+//  Fotmob_Clone
+//
+//  Created by Liam Horch on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Fotmob_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
